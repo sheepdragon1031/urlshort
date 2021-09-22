27f3202e9b7e2f72d9f058eec2d9dd7a1d092050
sheepdragon <dragonsheep1031@gmail.com>
2021-09-23 01:18:50 +0800

    feat: meta-tag output

--- frontend/pages/g/[...pid].js
+++ frontend/pages/g/[...pid].js
@@ -7,6 +7,7 @@ import Container from '@material-ui/core/Container';
 import axios from "axios";
 import Router from 'next/router'
 import { getSession, useSession, signIn, signOut } from "next-auth/client";
+import MetaTags from 'react-meta-tags';
 
 const Tourl = () => {
   const router = useRouter()
@@ -26,7 +27,7 @@ const Tourl = () => {
       }
     }
     else{
-      router.push({pathname: urlData['url2']})
+      router.push(urlData['url2'])
     }
     
   }, [pid, urlData])
@@ -38,6 +39,12 @@ const Tourl = () => {
     
         <Appbar />
         <Container maxWidth="sm">
+        <MetaTags>
+            <title>{urlData.url}</title>
+            <meta name="description" content={urlData.metatext} />
+            <meta property="og:title" content={urlData.metatitle} />
+            <meta property="og:image" content={urlData.metaimg} />
+          </MetaTags>
         {/* <h1>{pid}</h1> */}
         </Container>
     
