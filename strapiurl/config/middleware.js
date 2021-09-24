0e770c54a8f7ef9b9929872af571969d321cd245
sheepdragon <dragonsheep1031@gmail.com>
2021-09-24 16:57:28 +0800

    fix: CORS policy problem

---
+++ strapiurl/config/middleware.js
@@ -0,0 +1,14 @@
+module.exports = {
+    //...
+    settings: {
+      cors: {
+        origin: [
+        'http://localhost:3000', 
+        'http://d67b-36-233-248-148.ngrok.io',
+        'https://d67b-36-233-248-148.ngrok.io', 
+        'http://0490-36-233-248-148.ngrok.io',
+        'https://0490-36-233-248-148.ngrok.io'],
+      },
+    },
+  };
+   
